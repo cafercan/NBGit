c096766a5b45a61d9f937a15d719b5687e7ed08d
cafercan <cafercan@outlook.com>
2015-02-26 00:19:34 +0200

    v-7

--- NetBeansProjects/NBGit/index.php
+++ NetBeansProjects/NBGit/index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        print "deneme 2sadadas"
+        print "deneme 2sad"
         
         ?>
     </body>
